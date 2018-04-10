0a3b83c64278c1322a0071e85569be2453f02641
emil <lucasssemil@gmail.com>
2018-04-10 11:40:22 +0700

    gabungan on the fly

--- application/views/home.php
+++ application/views/home.php
@@ -100,143 +100,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			
 		</div>
 		<div id="checker_container" class="col m10">
-			<div class="row">
-				<div class="card-panel col m3 blue-grey lighten-3 z-depth-1" style="padding: 5px; margin: 0.5rem 0rem 0.1rem 0rem;">
-					<div class="meja row2 modal-trigger waves-effe  ct waves-light hea der" name="1" style="" href="#modal1">
-						<div class="col m6 font_cardheader blue-grey lighten-3 black-text" style="">TABLE : 1</div>
-						<div id="hbill1" class="col m6 font_cardheader blue-grey lighten-3 black-text" style="">BILL : 567512</div>
-						<div id="hmenu1" class="col m4 font_cardheader blue-grey lighten-3 black-text" style="">1/4</div>
-						<div id="hjam1" class="col m4 font_cardheader blue-grey lighten-3 black-text" style="">14:30:00</div>
-						<div id="hlama1" class="col m4 font_cardheader blue-grey lighten-3 black-text" style="">00:00</div>
-					</div>
-					<div class="row2 isi">
-						<div class="col m12" style="margin: 0px; padding:0px;">
-							<ul class="tabs " style="">
-								<li class="tab col m4"><a href="#test11"><i class="material-icons-card">play_circle_filled</i></a></li>
-								<li class="tab col m4"><a href="#test12"><i class="material-icons-card">stop</i></a></li>
-								<li class="tab col m4"><a href="#test13"><i class="material-icons-card">dashboard</i></a></li>
-								<!--<div class="indicator grey" style="z-index:1"> </div>-->
-							</ul>
-						</div>
-						<div id="test11" class="col m12" style="margin: 0px; padding:0px;">
-							<ul id='list11' class="collection white font_cardlist scrollable_list" style="">
-							</ul>
-						</div>
-						<div id="test12" class="col m12" style="margin: 0px; padding:0px;">
-							<ul id='list12' class="collection white font_cardlist scrollable_list">
-							</ul>
-						</div>
-						<div id="test13" class="col m12" style="margin: 0px; padding:0px;">
-							<ul id='list13' class="collection white font_cardlist scrollable_list">
-							</ul>
-						</div>
-						
-					</div>
-				</div>
-				<div class="card-panel col m3 blue-grey lighten-3 z-depth-1" style="padding: 5px; margin: 0.5rem 0rem 0.1rem 0rem;">
-					<div class="meja row2 modal-trigger waves-effe  ct waves-light header" name="2" style="" href="#modal1">
-						<div class="col m6 font_cardheader blue-grey lighten-3 black-text" style="">TABLE : 2</div>
-						<div id="hbill2" class="col m6 font_cardheader blue-grey lighten-3 black-text" style="">BILL : 567512</div>
-						<div id="hmenu2" class="col m4 font_cardheader blue-grey lighten-3 black-text" style="">1/4</div>
-						<div id="hjam2" class="col m4 font_cardheader blue-grey lighten-3 black-text" style="">14:30:00</div>
-						<div id="hlama2" class="col m4 font_cardheader blue-grey lighten-3 black-text" style="">00:00</div>
-					</div>
-					<div class="row2 isi">
-						<div class="col m12" style="margin: 0px; padding:0px;">
-							<ul class="tabs" style="">
-								<li class="tab col m4"><a href="#test21"><i class="material-icons-card">play_circle_filled</i></a></li>
-								<li class="tab col m4"><a href="#test22"><i class="material-icons-card">stop</i></a></li>
-								<li class="tab col m4"><a href="#test23"><i class="material-icons-card">dashboard</i></a></li>
-								
-							</ul>
-						</div>
-						<div id="test21" class="col m12" style="margin: 0px; padding:0px;">
-							<ul id='list21' class="collection font_cardlist scrollable_list">
-							</ul>
-						</div>
-						<div id="test22" class="col m12" style="margin: 0px; padding:0px;">
-							<ul id='list22' class="collection font_cardlist scrollable_list">
-								<li class="collection-item"> ORANGE JUICE</li>
-							</ul>
-						</div>
-						<div id="test23" class="col m12" style="margin: 0px; padding:0px;">
-							<ul id='list23' class="collection font_cardlist scrollable_list">
-								<li class="collection-item">2 CHIC TRYK BENTO</li>
-								<li class="collection-item">1 CHAWAN MUSHI</li>
-							</ul>
-						</div>
-					</div>
-				</div>
-				<div class="card-panel col m3 blue-grey lighten-3 z-depth-1" style="padding: 5px; margin: 0.5rem 0rem 0.1rem 0rem;">
-					<div class="meja row2 modal-trigger waves-effe  ct waves-light" name="3" style="" href="#modal1">
-						<div class="col m6 font_cardheader blue-grey lighten-3 black-text" style="">TABLE : 3</div>
-						<div id="hbill3" class="col m6 font_cardheader blue-grey lighten-3 black-text" style="">BILL : 567512</div>
-						<div id="hmenu3" class="col m4 font_cardheader blue-grey lighten-3 black-text" style="">1/4</div>
-						<div id="hjam3" class="col m4 font_cardheader blue-grey lighten-3 black-text" style="">14:30:00</div>
-						<div id="hlama3" class="col m4 font_cardheader blue-grey lighten-3 black-text" style="">00:00</div>
-					</div>
-					<div class="row2">
-						<div class="col m12" style="margin: 0px; padding:0px;">
-							<ul class="tabs" style="">
-								<li class="tab col m4"><a href="#test31"><i class="material-icons-card">play_circle_filled</i></a></li>
-								<li class="tab col m4"><a href="#test32"><i class="material-icons-card">stop</i></a></li>
-								<li class="tab col m4"><a href="#test33"><i class="material-icons-card">dashboard</i></a></li>
-								
-							</ul>
-						</div>
-						<div id="test31" class="col m12" style="margin: 0px; padding:0px;">
-							<ul id='list31' class="collection font_cardlist scrollable_list">
-							</ul>
-						</div>
-						<div id="test32" class="col m12" style="margin: 0px; padding:0px;">
-							<ul id='list32' class="collection font_cardlist scrollable_list">
-								<li class="collection-item"> ORANGE JUICE</li>
-							</ul>
-						</div>
-						<div id="test33" class="col m12" style="margin: 0px; padding:0px;">
-							<ul id='list33' class="collection font_cardlist scrollable_list">
-								<li class="collection-item">2 CHIC TRYK BENTO</li>
-								<li class="collection-item">1 CHAWAN MUSHI</li>
-							</ul>
-						</div>
-					</div>
-					
-				</div>
-				<div class="card-panel col m3 blue-grey lighten-3 z-depth-1" style="padding: 5px; margin: 0.5rem 0rem 0.1rem 0rem;">
-					<div class="meja row2 modal-trigger waves-effe  ct waves-light" name="4" style="" href="#modal1">
-						<div class="col m6 font_cardheader blue-grey lighten-3 black-text" style="">TABLE : 4</div>
-						<div id="hbill4" class="col m6 font_cardheader blue-grey lighten-3 black-text" style="">BILL : 567512</div>
-						<div id="hmenu4" class="col m4 font_cardheader blue-grey lighten-3 black-text" style="">1/4</div>
-						<div id="hjam4" class="col m4 font_cardheader blue-grey lighten-3 black-text" style="">14:30:00</div>
-						<div id="hlama4" class="col m4 font_cardheader blue-grey lighten-3 black-text" style="">00:00</div>
-					</div>
-					<div class="row2">
-						<div class="col m12" style="margin: 0px; padding:0px;">
-							<ul class="tabs" style="">
-								<li class="tab col m4"><a href="#test41"><i class="material-icons-card">play_circle_filled</i></a></li>
-								<li class="tab col m4"><a href="#test42"><i class="material-icons-card">stop</i></a></li>
-								<li class="tab col m4"><a href="#test43"><i class="material-icons-card">dashboard</i></a></li>
-								
-							</ul>
-						</div>
-						<div id="test41" class="col m12" style="margin: 0px; padding:0px;">
-						<ul id='list41' class="collection font_cardlist scrollable_list">
-							</ul>
-						</div>
-						<div id="test42" class="col m12" style="margin: 0px; padding:0px;">
-							<ul id='list42'  class="collection font_cardlist scrollable_list">
-								<li class="collection-item"> ORANGE JUICE</li>
-							</ul>
-						</div>
-						<div id="test43" class="col m12" style="margin: 0px; padding:0px;">
-							<ul id='list43' class="collection font_cardlist scrollable_list">
-								<li class="collection-item">2 CHIC TRYK BENTO</li>
-								<li class="collection-item">1 CHAWAN MUSHI</li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
 		</div>
 	</div>
 </div>
@@ -582,6 +445,116 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                     $('#list'+result[i]['nomormeja']+'3').empty();
                     $('#list'+result[i]['nomormeja']+'2').empty();
                 }
+                var ctrrow=1;
+                var tmp =0;
+                var ctr=1;
+                var ctrmeja=1;
+                
+                for(i=0;i<result.length;i++)
+                {
+                    if(tmp!=result[i]['nomormeja'])
+                    {
+                        //document.getElementById("hjam"+ctr).innerHTML = result[i]['jamorder'];
+                        tmp=result[i]['nomormeja'];  
+                        ctr = ctr+1;
+                    }
+                }
+                
+                //untuk inisiasi variabel progressbar dan countdown
+                ctr=1;
+                tmp=result[0]['nomormeja'];
+                
+                for(i=0;i<result.length;i++){
+                    
+                    if(tmp!=result[i]['nomormeja'])
+                    {
+                        ctr=1;
+                        tmp=result[i]['nomormeja'];
+                        
+                        if(tmp%4==1){
+                            ctrrow++;
+                        }
+                    }
+                    //mengeluarkan list pesanan
+                    //$('#test'+result[i]['nomormeja']+'1 ul').append('<li class="collection-item "><div class="col m9  ">'+result[i]['namamenurecipe']+'</div><div id="countdown'+result[i]['nomormeja']+ctr+'" class="col m2">0:0</div><div class="progress "><div id="pB'+result[i]['nomormeja']+ctr+'" class="determinate " style="width:0%"></div></div></li>');
+                    
+                    if (!document.getElementById("meja"+result[i]['nomormeja'])) {
+                        if (!document.getElementById("rowid"+ctrrow)){
+                            $('#checker_container').append('<div class="row" id="rowid'+ctrrow+'"></div>');
+                        }
+                            $('#rowid'+ctrrow).append('<div id="meja'+result[i]['nomormeja']+'" class="card-panel col m3 blue-grey lighten-3 z-depth-1" style="padding: 5px; margin: 0.5rem 0rem 0.1rem 0rem;">'+
+                        '<div class="meja row2 modal-trigger waves-effe  ct waves-light header" name="'+result[i]['nomormeja']+'" style="" href="#modal1">'+
+                            '<div class="col m6 font_cardheader blue-grey lighten-3 black-text" style="">TABLE : '+result[i]['nomormeja']+'</div>'+
+                            '<div id="hbill'+result[i]['nomormeja']+'" class="col m6 font_cardheader blue-grey lighten-3 black-text" style="">BILL : 567512</div>'+
+                            '<div id="hmenu'+result[i]['nomormeja']+'" class="col m4 font_cardheader blue-grey lighten-3 black-text" style="">1/4</div>'+
+                            '<div id="hjam'+result[i]['nomormeja']+'" class="col m4 font_cardheader blue-grey lighten-3 black-text" style="">'+result[i]['jamorder']+'</div>'+
+                            '<div id="hlama'+result[i]['nomormeja']+'" class="col m4 font_cardheader blue-grey lighten-3 black-text" style="">00:00</div>'+
+                        '</div>'+
+                        '<div class="row2 isi" id="rowlist'+result[i]['nomormeja']+'">'+
+  
+                        '</div>'+
+                    '</div>');
+                    $('#rowlist'+result[i]['nomormeja']).append('<div class="col m12" style="margin: 0px; padding:0px;">'+
+                                '<ul class="tabs" id="tabs'+result[i]['nomormeja']+'" style="">'+
+                                    '<li class="tab col m4"><a href="#test'+result[i]['nomormeja']+'1"><i class="material-icons-card">play_circle_filled</i></a></li>'+
+                                    '<li class="tab col m4"><a href="#test'+result[i]['nomormeja']+'2"><i class="material-icons-card">stop</i></a></li>'+
+                                    '<li class="tab col m4"><a href="#test'+result[i]['nomormeja']+'3"><i class="material-icons-card">dashboard</i></a></li>'+
+                                '</ul>'+
+                            '</div>');
+                    $('#rowlist'+result[i]['nomormeja']).append('<div id="test'+result[i]['nomormeja']+'1" class="col m12" style="margin: 0px; padding:0px;">'+
+                                '<ul id="list'+result[i]['nomormeja']+'1" class="collection white font_cardlist scrollable_list" style="">'+
+                                '</ul>'+
+                            '</div>');
+                    $('#rowlist'+result[i]['nomormeja']).append('<div id="test'+result[i]['nomormeja']+'2" class="col m12" style="margin: 0px; padding:0px;">'+
+                                '<ul id="list'+result[i]['nomormeja']+'2" class="collection white font_cardlist scrollable_list">'+
+                                '</ul>'+
+                            '</div>');
+                    $('#rowlist'+result[i]['nomormeja']).append('<div id="test'+result[i]['nomormeja']+'3" class="col m12" style="margin: 0px; padding:0px;">'+
+                                '<ul id="list'+result[i]['nomormeja']+'3" class="collection white font_cardlist scrollable_list">'+
+                                '</ul>'+
+                            '</div>');
+                        
+                    var $tabs = $('#tabs'+result[i]['nomormeja']);
+                    $tabs.children().removeAttr('style');   
+                        
+                    }
+                    if (result[i]['status']==0){
+                        $('#test'+result[i]['nomormeja']+'3 ul').append('<li class="collection-item "><div class="col m9 ">'+result[i]['namamenurecipe']+'</div><div id="countdowna'+result[i]['nomormeja']+ctr+'" class="col m2">0:0</div><div class="progress "><div id="pBa'+result[i]['nomormeja']+ctr+'"  class="determinate" style="width:0%"></div></div></li>');
+                        $('#test'+result[i]['nomormeja']+'1 ul').append('<li class="collection-item "><div class="col m9 ">'+result[i]['namamenurecipe']+'</div><div id="countdown'+result[i]['nomormeja']+ctr+'" class="col m2">0:0</div><div class="progress "><div id="pB'+result[i]['nomormeja']+ctr+'"  class="determinate" style="width:0%"></div></div></li>');
+                        ctr = ctr+1;
+                    } 
+                   if(result[i]['status']==1){
+                        var sisawaktu = hitungSisawaktu(result[i]['jamtarget'],result[i]['jamfinish']);
+                        $('#test'+result[i]['nomormeja']+'3 ul').append('<li class="collection-item "><div class="strikeout col m9">'+result[i]['namamenurecipe']+'</div><div>'+sisawaktu+'</div></li>');
+                        $('#test'+result[i]['nomormeja']+'2 ul').append('<li class="collection-item "><div class="strikeout col m9">'+result[i]['namamenurecipe']+'</div><div>'+sisawaktu+'</div></li>');
+                    }
+                    
+                    $tabs.tabs().tabs('select_tab', 'test'+result[i]['nomormeja']+'2');
+                    $tabs.tabs().tabs('select_tab', 'test'+result[i]['nomormeja']+'3');
+                    $tabs.tabs().tabs('select_tab', 'test'+result[i]['nomormeja']+'1');
+                    //alert("meja"+tmp+"ctr"+ctr);    
+                }
+			}, error: function(msg){
+				alert('Reload Data Error');
+			}
+		});
+	}
+	/*function reloadalldata(){
+		//alert('a1');
+		$.ajax({
+			type:"GET", 
+			dataType: 'json',
+			url: "<?php echo site_url(); ?>" + "/Controller/get_table", 
+			data:{ "mode":"resep" },                                     
+			success :function(result){
+				//$('#container').html(JSON.stringify(result));
+                //$('#test'+result[0]['nomormeja']+'1').html(JSON.stringify(result));
+                //alert(totmeja);
+                for(i=0;i<result.length;i++){
+                    $('#list'+result[i]['nomormeja']+'1').empty();
+                    $('#list'+result[i]['nomormeja']+'3').empty();
+                    $('#list'+result[i]['nomormeja']+'2').empty();
+                }
              
                 var tmp =0;
                 var ctr=1;
@@ -627,7 +600,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 				alert('Reload Data Error');
 			}
 		});
-	}
+	}*/
     
 	 $(document).ready(function(){
 	// // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
